782a53c5b53e7fe563ce0974baa712f3b3247bad
Sven A. Schmidt <sas@finestructure.co>
2024-05-31 10:08:35 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "s3-uploader",
+    targets: [
+        .executableTarget(name: "s3-uploader"),
+    ]
+)
