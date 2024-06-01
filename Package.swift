ec5a2a1c45648ac1e876e60ecaf189605a3a3762
Sven A. Schmidt <sas@finestructure.co>
2024-06-01 11:23:34 +0200

    Add Soto dependency to uploade

--- Package.swift
+++ Package.swift
@@ -6,11 +6,13 @@ let package = Package(
     name: "s3-uploader",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "https://github.com/soto-project/soto-s3-file-transfer.git", from: "1.3.0")
     ],
     targets: [
         .executableTarget(name: "s3-uploader", dependencies: [
-            .product(name: "ArgumentParser", package: "swift-argument-parser")
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "SotoS3FileTransfer", package: "soto-s3-file-transfer")
         ]),
     ]
 )
--- Sources/Entrypoint.swift
+++ Sources/Entrypoint.swift
@@ -1,4 +1,5 @@
 import ArgumentParser
+import SotoS3FileTransfer
 
 
 @main
@@ -10,8 +11,32 @@ struct S3Uploader: AsyncParsableCommand {
         guard paths.count == 2 else {
             throw ValidationError("Must have exactly 2 arguments: <input path> <S3 path>")
         }
-        let inputPath = paths[0]
-        let s3Path = paths[1]
-        print(inputPath, "->", s3Path)
+        try await Self.upload(from: paths[0], to: paths[1])
+    }
+
+    static func upload(from inputPath: String, to s3Path: String) async throws {
+        let client = AWSClient(httpClientProvider: .createNew)
+        defer { try? client.syncShutdown() }
+
+        let s3 = S3(client: client, region: .useast2)
+        let s3FileTransfer = S3FileTransferManager(s3: s3, threadPoolProvider: .singleton)
+
+        guard let s3File = S3File(url: s3Path) else {
+            throw Error(message: "Filed to construct S3File for url: \(s3Path)")
+        }
+
+        print("Uploading \(inputPath) -> \(s3Path) ...")
+        do {
+            try await s3FileTransfer.copy(from: inputPath, to: s3File).get()
+            print("Done.")
+        } catch {
+            print("Error while uploading file: \(error)")
+        }
+    }
+
+    struct Error: Swift.Error {
+        let message: String
+
+        var localizedDescription: String { message }
     }
 }
