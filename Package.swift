6075f2cbe7b23b9514dd11dda5d83b742716ba8c
Sven A. Schmidt <sas@finestructure.co>
2024-06-01 13:50:11 +0200

    Add basic Alamofire fetch

--- Package.swift
+++ Package.swift
@@ -6,11 +6,13 @@ let package = Package(
     name: "s3-uploader",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1")
     ],
     targets: [
         .executableTarget(name: "s3-uploader", dependencies: [
-            .product(name: "ArgumentParser", package: "swift-argument-parser")
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "Alamofire", package: "alamofire")
         ]),
     ]
 )
--- Sources/Entrypoint.swift
+++ Sources/Entrypoint.swift
@@ -1,3 +1,4 @@
+import Alamofire
 import ArgumentParser
 
 
@@ -10,8 +11,15 @@ struct S3Uploader: AsyncParsableCommand {
         guard paths.count == 2 else {
             throw ValidationError("Must have exactly 2 arguments: <input path> <S3 path>")
         }
-        let inputPath = paths[0]
-        let s3Path = paths[1]
-        print(inputPath, "->", s3Path)
+
+        try await fetch()
+    }
+
+    func fetch() async throws {
+        let url = "https://raw.githubusercontent.com/finestructure/s3-uploader/main/Package.swift"
+        print("Fetching \(url)...")
+
+        let value = try await AF.request("https://raw.githubusercontent.com/finestructure/s3-uploader/main/Package.swift").serializingString().value
+        print(value)
     }
 }
